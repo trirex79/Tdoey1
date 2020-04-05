aa50a6a2e66889400e810c39e7b54b22ed4a09a1
karolina <trirex79@gmail.com>
2020-04-05 21:32:38 +0200

    Initial Commit

---
+++ Tdoey1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tdoey1
+//
+//  Created by Karolina on 05.04.2020.
+//  Copyright © 2020 KarolinaLeg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
